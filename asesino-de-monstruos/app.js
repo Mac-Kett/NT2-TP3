1cf7b7220c015244cf6eb7b54d52ed74411fb741
Macarena Quiroz Kettle <56402915+Mac-Kett@users.noreply.github.com>
2021-05-04 23:03:41 -0300

    innitial commit

---
+++ asesino-de-monstruos/app.js
@@ -0,0 +1,101 @@
+new Vue({
+    el: '#app',
+    data: {
+        saludJugador: 100,
+        saludMonstruo: 100,
+        hayUnaPartidaEnJuego: false,
+        turnos: [], //es para registrar los eventos de la partida
+        esJugador: false,
+        rangoAtaque: [3, 10],
+        rangoAtaqueEspecial: [10, 20],
+        rangoAtaqueDelMonstruo: [5, 12],
+    },
+
+    methods: {
+        getSalud(salud) {
+            return `${salud}%`
+        },
+        empezarPartida: function () {
+            this.hayUnaPartidaEnJuego=true;
+            this.saludJugador=100;
+            this.saludMonstruo=100;
+            this.turnos.length=0;
+        },
+        atacar: function () {
+            let danioCausado = this.calcularHeridas(3,10);
+            this.saludMonstruo= this.saludMonstruo - danioCausado;
+            this.turnos.unshift({
+                esJugador: true,
+                text: "Se golpeó al monstrue con" + danioCausado + "puntos"
+            })
+            if(this.verificarGanador()) {
+                return;
+            }
+            this.ataqueDelMonstruo();
+        },
+
+        ataqueEspecial: function () {
+            let danioCausado = this.calcularHeridas(10,20);
+            this.saludJugador -= danioCausado;
+            if(this.verificarGanador()) {return;}
+            this.ataqueDelMonstruo();
+        },
+
+        curar: function () {
+            if(this.saludJugador <= 90){
+                this.saludJugador+= 10;
+            } else {
+                this.ataqueDelMonstruo();
+            }
+        },
+
+        registrarEvento(evento) {
+
+        },
+        terminarPartida: function () {
+            this.hayUnaPartidaEnJuego = false;
+            this.turnos.length = 0;
+        },
+
+        ataqueDelMonstruo: function () {
+            let danioCausado = this.calcularHeridas(5,12);
+            this.saludJugador -= danioCausado;
+            this.turnos.unshift({
+                esJugador: true,
+                text: "El jugador fue golpeado con " + danioCausado + "puntos"
+            })
+            this.verificarGanador();
+        },
+
+        calcularHeridas: function (min, max) {
+            return Math.max(Math.floor(Math.random()*max)+1, min);
+
+        },
+        verificarGanador: function () {
+            if(this.saludMonstruo <= 0){
+                if(confirm('Ganaste!')){
+                    this.empezarPartida();
+                } else {
+                    this.hayUnaPartidaEnJuego = false;
+                }
+                return true;
+            } else if(this.saludJugador <= 0){
+                if(confirm('Perdiste!')){
+                    this.empezarPartida();
+                } else {
+                    this.hayUnaPartidaEnJuego = false;
+                }
+                return true;
+            }
+
+            return false;
+        },
+        cssEvento(turno) {
+            //Este return de un objeto es prque vue asi lo requiere, pero ponerlo acá queda mucho mas entendible en el codigo HTML.
+            return {
+                'player-turno': turno.esJugador,
+                'monster-turno': !turno.esJugador
+            }
+        }
+    }
+});
